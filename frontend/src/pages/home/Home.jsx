e5cb4379548603807ea25c7b7101c189a76cf470
Joyce Tsai <ninomii1126@gmail.com>
2024-11-05 10:58:14 -0800

    frontend:add recommended section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSeller from './TopSeller'
+import Recommended from './Recommended'
 
 const Home = () => {
   return (
     <>
     <Banner/>
     <TopSeller/>
+    <Recommended/>
     </>
   )
 }
---
+++ frontend/src/pages/home/Recommended.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+
+import BookCard from "../books/BookCard";
+
+const Recommended = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  return (
+    <div className="py-16">
+      <h2 className="text-3x1 font-semibold mb-6">Recommended for you</h2>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {books.length > 0 &&
+          books.slice(8, 18).map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard key={index} book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Recommended;
