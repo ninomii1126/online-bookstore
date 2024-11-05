a9d77a7801b50a0446631c1d7c13bb228f3b8106
Joyce Tsai <ninomii1126@gmail.com>
2024-11-05 12:13:18 -0800

    frontend: add news section to home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 import TopSeller from './TopSeller'
 import Recommended from './Recommended'
+import News from './News'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Banner/>
     <TopSeller/>
     <Recommended/>
+    <News/>
     </>
   )
 }
