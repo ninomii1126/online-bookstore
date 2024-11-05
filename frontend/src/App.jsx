91ca12d0c048293c26fc37086fcfdd9b4b5494a4
Joyce Tsai <ninomii1126@gmail.com>
2024-11-05 12:21:34 -0800

    frontend: add footer section to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary' >
       <Outlet/>
       </main>
-      <footer>footer</footer>
+      <Footer/>
     </>
   )
 }
