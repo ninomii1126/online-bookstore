33fb2a6976dd5f815e251b6da1968321900353ce
Joyce Tsai <ninomii1126@gmail.com>
2024-11-13 16:38:27 -0800

    modified add to cart pop up window using sweetalert2

--- frontend/src/redux/features/counter/cartSlice.js
+++ frontend/src/redux/features/counter/cartSlice.js
@@ -1,30 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
 
 const initialState = {
   cartItems: [],
-}
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState:initialState,
-    reducers:{
-        addToCart:(state, action)=>{
-             const existingItems = state.cartItems.find(item => 
-                item._id===action.payload._id
-    
-             ) 
-             if(!existingItems){
-                state.cartItems.push(action.payload)
-                alert("Item is added sucessfully!")
-                
-            }else{
-                alert("Item already exist!")
-            }
-        }
-    }
-})
+  name: "cart",
+  initialState: initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItems = state.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+      if (!existingItems) {
+        state.cartItems.push(action.payload);
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Item is added",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Item already exist!",
+        });
+      }
+    },
+  },
+});
 
 // export actions
-export const {addToCart} = cartSlice.actions
+export const { addToCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
