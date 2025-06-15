c3c97324e0814392a76c591c38f1ef8aeb2094cd
Joyce Tsai <ninomii1126@gmail.com>
2025-06-15 11:29:43 -0700

    add forward controller

--- backend/src/main/java/com/Joyce/bookstore/controller/TestController.java
+++ backend/src/main/java/com/Joyce/bookstore/controller/TestController.java
@@ -1,12 +1,12 @@
 package com.Joyce.bookstore.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @RequestMapping(value = "/{[path:[^\\.]*}")
     public String index() {
        return "forward:/index.html";
     }
