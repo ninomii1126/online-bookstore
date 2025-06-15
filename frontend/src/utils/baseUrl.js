bb3f0daa0eb4587232efddc2d57fab65e6f71adc
Joyce Tsai <ninomii1126@gmail.com>
2025-06-15 11:12:05 -0700

    frontend url

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,5 +1,8 @@
 const getBaseUrl = () =>{
-    return "http://localhost:8080"
+//    return "http://localhost:8080"
+    return "https://online-bookstore-production-9e09.up.railway.app"
+
+    
 }
 
 export default getBaseUrl;
