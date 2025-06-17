2c16eb6328962efcdcacaaf0d6247f157542321f
Joyce Tsai <ninomii1126@gmail.com>
2025-06-16 17:38:08 -0700

    base url use env variable

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () =>{
-//    return "http://localhost:8080"
-    return "https://online-bookstore-production-9e09.up.railway.app"
+    return "import.meta.env.VITE_API_URL"
+    //return "https://online-bookstore-production-9e09.up.railway.app"
 
     
 }
