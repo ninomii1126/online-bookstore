c1237356406f32c93a4b32171935fce99e6cfd32
Chun <77891295+ninomii1126@users.noreply.github.com>
2025-07-14 15:05:28 -0700

    Update firebase.config.jsx

--- frontend/src/firebase/firebase.config.jsx
+++ frontend/src/firebase/firebase.config.jsx
@@ -21,7 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Firebase Analytics 不建議在 SSR 執行
+// Firebase Analytics 不建議在SSR執行
 if (typeof window !== "undefined") {
   import("firebase/analytics").then(({ getAnalytics }) => {
     getAnalytics(app);
